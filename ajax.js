cc7c83522e0eb8991a431181e1723ffa0778df87
@jonebalo <jonebalo@hotmail.com>
2023-06-07 17:36:10 +0200

    AJAX

--- ajax.js
+++ ajax.js
@@ -1,24 +1,57 @@
-// const http = new XMLHttpRequest();
+const http = new XMLHttpRequest();
 
-// http.onreadystatechange = function (){
+http.onreadystatechange = function (){
 
-//     console.log(this.readyState);
+    console.log(this.readyState);
 
-//     if (this.readyState === 4 && this.status === 200) {
+    if (this.readyState === 4 && this.status === 200) {
 
-//         console.log(this.responseText);
-//         const entradas = JSON.parse (this.responseText);
-//     }
-// }
+        console.log(this.responseText);
+        const entradas = JSON.parse (this.responseText);
+    }
+}
 
-// http.open("GET","https://api.publicapis.org/entries");
-// http.send ();
+http.open("GET","https://api.publicapis.org/entries");
+http.send ();
+
+// // function inicializar(){
+// //     console.log("onload se ha llamado");
+// //     // Instancia a la clase XML...
+
+// //     const http = new XMLHttpRequest();
+// // // Método
+// // // onreadystatechange
+// // http.onreadystatechange = function(){
+// //     // Nos está sacando 1,2,3,4
+// //     console.log(this.readyState);
+// //     // readystate es el estado del pedido
+// //     if(this.readyState === 4 && this.status === 200){
+// //         // imprimir por consola un array de entries
+// //         console.log(this.responseText);
+// //         const entradas = JSON.parse(this.responseText);
+// //         new ListaEntradas(entradas.entries).render();
+// //         }
+// // }
+// // http.open("GET", "https://api.publicapis.org/entries");
+// // http.send();
+// //     }
+// // console.log(this.responseText);
+// // function ListaEntradas(entradas){
+// //     this.entradas = entradas;
+// //     this.render = function(){
+// //         let listaDiv = document.getElementById("entradas");
+// //         listaDiv.innerHTML = "<ol>";
+// //         for(const entradas of this.entradas){
+// //         listaDiv.innerHTML += `<li> ${entradas.API} ${entradas.Description}  ${entradas.Category} </li>`;
+// //         }
+// //         listaDiv.innerHTML +=  "</ol>";
+// //     }
+// // };
 
 // function inicializar(){
 //     console.log("onload se ha llamado");
 //     // Instancia a la clase XML...
-
-//     const http = new XMLHttpRequest();
+// const http = new XMLHttpRequest();
 // // Método
 // // onreadystatechange
 // http.onreadystatechange = function(){
@@ -41,42 +74,9 @@
 //     this.render = function(){
 //         let listaDiv = document.getElementById("entradas");
 //         listaDiv.innerHTML = "<ol>";
-//         for(const entradas of this.entradas){
-//         listaDiv.innerHTML += `<li> ${entradas.API} ${entradas.Description}  ${entradas.Category} </li>`;
+//         for(const entrada of this.entradas){
+//         listaDiv.innerHTML += `<li> ${entrada.API} ${entrada.Description}  ${entrada.Category} </li>`;
 //         }
 //         listaDiv.innerHTML +=  "</ol>";
 //     }
-// };
-
-function inicializar(){
-    console.log("onload se ha llamado");
-    // Instancia a la clase XML...
-const http = new XMLHttpRequest();
-// Método
-// onreadystatechange
-http.onreadystatechange = function(){
-    // Nos está sacando 1,2,3,4
-    console.log(this.readyState);
-    // readystate es el estado del pedido
-    if(this.readyState === 4 && this.status === 200){
-        // imprimir por consola un array de entries
-        console.log(this.responseText);
-        const entradas = JSON.parse(this.responseText);
-        new ListaEntradas(entradas.entries).render();
-        }
-}
-http.open("GET", "https://api.publicapis.org/entries");
-http.send();
-    }
-console.log(this.responseText);
-function ListaEntradas(entradas){
-    this.entradas = entradas;
-    this.render = function(){
-        let listaDiv = document.getElementById("entradas");
-        listaDiv.innerHTML = "<ol>";
-        for(const entrada of this.entradas){
-        listaDiv.innerHTML += `<li> ${entrada.API} ${entrada.Description}  ${entrada.Category} </li>`;
-        }
-        listaDiv.innerHTML +=  "</ol>";
-    }
-}; 
+// }; 
---
+++ nuevo.js
@@ -0,0 +1,73 @@
+function init(numeroPaginas = 0) {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let response = JSON.parse(this.responseText);
+        console.log(response);
+        new Datos(response).render();
+      }
+    };
+    let url = `https://dummyapi.io/data/v1/post?limit=${limite}&page=${numeroPaginas}`;
+    xhr.open(“GET”, url, true);
+    xhr.setRequestHeader(“app-id”, “64762ccc042da10aab777784”);
+    xhr.send();
+  }
+  function Datos(response) {
+    this.response = response;
+    const mascotasDiv = document.getElementById(“mascotas”);
+    const paginasDiv = document.getElementById(“btn”);
+    this.render = function () {
+      mascotasDiv.innerHTML = “”;
+      for (const items of this.response.data) {
+        mascotasDiv.innerHTML += `<li>
+              <img src=“${items.owner.picture}” width=“60px”/>
+              ${items.owner.title} ${items.owner.firstName} ${items.owner.lastName}
+              ${items.publishDate}
+              <img src=“${items.image}” width=“100"/>
+              ${items.publishDate}
+              ${items.text}
+              ${items.likes}</li>`;
+          for (const tag of items.tags){
+              mascotasDiv.innerHTML += `<a href=“tags.html?tags=${tag}“>${tag}</a>`;
+          }
+      }
+      paginasDiv.innerHTML = “”;
+      for (let i = 1; i <= this.response.total / limite; i++) {
+        paginasDiv.innerHTML += `<a href=“#” onclick=“init(${i})“>${i}</a> `;
+      }
+    };
+  }
+  function tags() {
+    let userTags = new URLSearchParams(window.location.search).get(“tags”);
+    let tagsDiv = document.getElementById(“tags”);
+    let xhr = new XMLHttpRequest();
+    let paginasDiv= document.getElementById(“paginas”);
+    xhr.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+        let usuarios = JSON.parse(this.responseText);
+        tagsDiv.innerHTML = “”;
+        for (const items of usuarios.data) {
+          tagsDiv.innerHTML += `
+              <img src=“${items.owner.picture}” width=“60px”/>
+              <p>${items.owner.title} ${items.owner.firstName} ${items.owner.lastName}</p>
+              <p>${items.publishDate}</p>
+              <img src=“${items.image}” width=“100"/>
+              <p>${items.publishDate}</p>
+              <p>${items.text}</p>
+              <p>${items.likes}</p>`;
+              for (const tag of items.tags){
+                  tagsDiv.innerHTML += `<a href=“tags.html?tags=${tag}“>${tag}</a>`;
+              }
+        }
+        paginasDiv.innerHTML = “”;
+        for (let i = 1; i <= this.response.total / limite; i++) {
+          paginasDiv.innerHTML += `<a href=“#” onclick=“init(${i})“>${i}</a> `;
+        }
+      }
+    };
+    let url = `https://dummyapi.io/data/v1/tag/${userTags}/post?limit=${limite}`;
+    xhr.open(“GET”, url, true);
+    xhr.setRequestHeader(“app-id”, “64762ccc042da10aab777784”);
+    xhr.send();
+  }
